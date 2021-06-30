0532f618046aeae2fa297cdaa5d5a954ab139686
Viktoria <doduchenko29@gmail.com>
2021-06-30 19:45:10 +0300

    add

---
+++ index.js
@@ -0,0 +1,39 @@
+/*const fetchUser = (userID) => {
+  return fetch(`https://api.github.com/users/${userID}`)
+    .then((response) => response.json())
+    .then((res) => res.blog);
+};
+
+const getUsersBlog = async (users) => {
+  const promises = users.map(async (userID) => {
+    const res = await fetchUser(userID);
+    return res;
+  });
+  return Promise.all(promises);
+};
+getUsersBlog(['google', 'facebook']).then((linksList) =>
+  console.log(linksList)
+);*/
+
+const fetchUser = (userID) => {
+  return fetch(`https://api.github.com/users/${userID}`)
+    .then((response) => response.json())
+    .then((res) => res.blog);
+};
+
+export const getUsersBlog = async (users) => {
+  try {
+    const promises = users.map(async (userID) => {
+      const res = await fetchUser(userID);
+      return res;
+    });
+    return Promise.all(promises);
+  } catch {
+    return Promise.reject()
+  }
+  
+};
+getUsersBlog(['google', 'facebook']).then((linksList) =>
+  console.log(linksList)
+);
+
